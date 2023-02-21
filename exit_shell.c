7ad77b8c7396beeb7217c810dc41db61a730df8d
Dennis Muvaa <dmuvaa70@gmail.com>
2023-02-21 10:45:54 -0800

    Exit Shell

---
+++ exit_shell.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void exit_shell(char* arg)
+{
+	int status = 0;
+
+	if (arg != NULL)
+	{
+		status = atoi(arg);
+
+		if (status == 0 && strcmp(arg, "0") != 0)
+		{
+			printf("Error: exit status must be an integer\n");
+			exit(1);
+		}
+	}
+	exit(status);
+}
