480707d2e1a088bf97a51527480d12e743ace3b6
Dennis Muvaa <dmuvaa70@gmail.com>
2023-02-22 09:42:30 -0800

    main mainL

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <sys/stat.h>
+
+extern char **environ;
+
+#define BUFFER_SIZE 1024
+
+int handle_args(char *command, char **arr);
+int check_builtins(char *command);
+void execute_builtin(char **arr);
+int execute_command(char **arr, char **env);
+void print_env(char **env);
+char *get_path(char *command, char **env);
+char *get_path(char *command, char **env);
+void execute_builtin(char **args);
+
+#endif
