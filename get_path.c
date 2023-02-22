24638ac77e6454686837a5f497afd431b2182023
Janipher <janipherowiny@gmail.com>
2023-02-22 09:40:48 -0800

    Modifications

--- get_path.c
+++ get_path.c
@@ -4,7 +4,7 @@
  * get_path - Searches the PATH for the given
  * command
  * @command: The command to search for
- *
+ * @env: variable
  * Return: The full path of the command found,
  * NULL otherwise
  */
@@ -14,21 +14,21 @@ char *get_path(char *command, char **env)
 	char *path, *tok, *tmp, *path_cpy;
 	struct st;
 
-	path = _getenv("PATH", env);
+	path = getenv("PATH", env);
 	if (path == NULL)
 		return (NULL);
 
-	path_cpy = _strdup(path);
+	path_cpy = strdup(path);
 	if (path_cpy == NULL)
 	{
 		free(path);
 		return (NULL);
 	}
-	tok = strtok(path_cpy,":");
+	tok = strtok(path_cpy, ":");
 	while (tok != NULL)
 	{
-		tmp = _strcat(tok, "/");
-		tmp = _strcat(tmp, command);
+		tmp = strcat(tok, "/");
+		tmp = strcat(tmp, command);
 		if (stat(tmp, &st) == 0)
 		{
 			free(path);
